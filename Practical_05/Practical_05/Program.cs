640481a5461c350744b3fde02b3530992e24a53a
dharvi_kaneriya <dharvikaneriya@gmail.com>
2025-03-30 11:39:39 +0530

    feature:Implemented the practical on try catch and finally block.

---
+++ Practical_05/Practical_05/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practical_05
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = {1,2,3,4,5};
+
+
+            for (int i = 0; i <= 5; i++)  // Intentionally going out of bounds
+            {
+                try
+                {
+                    Console.WriteLine($"Value at index {i}: {numbers[i]}");
+                }
+                catch (IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine($"Exception caught: {ex.Message}");
+                }
+                finally
+                {
+                    Console.WriteLine("This is finally block");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
